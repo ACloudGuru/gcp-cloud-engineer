495272846690c9d4af58530e94f406285e842232
Mattias Andersson <github@andersson.ca>
2018-06-07 16:50:58 -0700

    Add Hello World apps for Cloud Shell lab

---
+++ cloud-shell-hello/hello.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 8080;
+
+const message = 'Hello Cloud Gurus!'
+const fullMessage = `${message}\n`
+
+const server = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end(fullMessage);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
---
+++ cloud-shell-hello/html-hello.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 8081;
+
+const message = 'Hello Cloud Gurus and welcome to this lab!'
+const fullMessage = `<html><body><h1>${message}</h1></body></html>\n`
+
+const server = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/html');
+  response.end(fullMessage);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
