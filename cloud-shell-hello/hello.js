8267d9960a5c905b0e2a1239c06c42709976e2e9
Mattias Andersson <github@andersson.ca>
2018-06-09 10:31:33 -0700

    Add EOL semicolons that I was silly and forgot

--- cloud-shell-hello/hello.js
+++ cloud-shell-hello/hello.js
@@ -3,8 +3,8 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 8080;
 
-const message = 'Hello Cloud Gurus!'
-const fullMessage = `${message}\n`
+const message = 'Hello Cloud Gurus!';
+const fullMessage = `${message}\n`;
 
 const server = http.createServer((request, response) => {
   response.statusCode = 200;
--- cloud-shell-hello/html-hello.js
+++ cloud-shell-hello/html-hello.js
@@ -3,8 +3,8 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 8081;
 
-const message = 'Hello Cloud Gurus and welcome to this lab!'
-const fullMessage = `<html><body><h1>${message}</h1></body></html>\n`
+const message = 'Hello Cloud Gurus and welcome to this lab!';
+const fullMessage = `<html><body><h1>${message}</h1></body></html>\n`;
 
 const server = http.createServer((request, response) => {
   response.statusCode = 200;
