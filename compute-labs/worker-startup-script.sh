9c92b7f7158953f33aaaff295c62a91923db635f
Mattias Andersson <github@andersson.ca>
2018-09-19 13:58:24 -0700

    Add startup script for GCE challenge lab

---
+++ compute-labs/worker-startup-script.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+
+#
+# Echo commands as they are run, to make debugging easier.
+# GCE startup script output shows up in "/var/log/syslog" .
+#
+set -x
+
+
+#
+# Make sure installed packages are up to date with all security patches.
+#
+apt-get update
+
+
+#
+# Install Google's Stackdriver logging agent, as per
+# https://cloud.google.com/logging/docs/agent/installation
+#
+curl -sSO https://dl.google.com/cloudagents/install-logging-agent.sh
+bash install-logging-agent.sh
+
+
+#
+# Install and run the "stress" tool to max the CPU load for a while.
+#
+apt-get install stress
+stress -c 8 -t 120
+
+
+#
+# Report that we're done.
+#
+
+# Metadata should be set in the "lab-logs-bucket" attribute using the "gs://mybucketname/" format.
+log_bucket_metadata_name=lab-logs-bucket
+log_bucket_metadata_url="http://metadata.google.internal/computeMetadata/v1/instance/attributes/${log_bucket_metadata_name}"
+worker_log_bucket=$(curl -H "Metadata-Flavor: Google" "${log_bucket_metadata_url}")
+
+# We write a file named after this machine.
+worker_log_file="machine-$(hostname)-finished.txt"
+echo "Phew!  Work completed at $(date)" >"${worker_log_file}"
+
+# And we copy that file to the bucket specified in the metadata.
+echo "Copying the log file to the bucket..."
+gsutil cp "${worker_log_file}" "${worker_log_bucket}"
