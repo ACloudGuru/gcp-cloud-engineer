40cf6fdc10b1c4cf7840545519a20bca006a1074
cpalreddy06 <74147570+cpalreddy06@users.noreply.github.com>
2021-02-16 17:15:41 -0500

    Update hello.js

--- cloud-shell-hello/hello.js
+++ cloud-shell-hello/hello.js
@@ -3,7 +3,7 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 8080;
 
-const message = 'Hello Cloud Gurus!';
+const message = 'Hello Cloud Gurus, it is awesome!';
 const fullMessage = `${message}\n`;
 
 const server = http.createServer((request, response) => {
